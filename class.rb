ff7848c8df22f9c835e4478e7b196687a9ce1f42
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-01-20 23:00:50 +0100

    Create enumerables module

---
+++ class.rb
@@ -0,0 +1,21 @@
+class MyList
+  include MyEnumerable
+
+  def initialize(*List)
+    @list = list
+  end
+
+  def each
+    (0...@list.length).each do |i|
+      yield @list[i]
+    end
+  end
+end
+
+list = MyList.new(1, 2, 3, 4)
+
+puts(list.all_e? { |e| e < 5})
+puts(list.all_e? { |e| e > 5})
+puts(list.all_e? { |e| e == 2})
+puts(list.all_e? { |e| e == 5})
+puts(list.filter_e?(&:even?)
