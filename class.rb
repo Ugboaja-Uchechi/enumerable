a95aa735e5608f3ebf141b07fb92455881acd758
luftedar <ugurorcun91@gmail.com>
2022-01-21 10:16:14 +0300

    Fixed Linters

--- class.rb
+++ class.rb
@@ -1,23 +1,23 @@
 require_relative './enumerable'
 
-# class MyList
-#   include MyEnumerable
+class MyList
+  include MyEnumerable
 
-#   def initialize(*list)
-#     @list = list
-#   end
+  def initialize(*list)
+    @list = list
+  end
 
-#   def each
-#     (0...@list.length).each do |i|
-#       yield @list[i]
-#     end
-#   end
-# end
+  def each
+    (0...@list.length).each do |i|
+      yield @list[i]
+    end
+  end
+end
 
-# list = MyList.new(1, 2, 3, 4)
+list = MyList.new(1, 2, 3, 4)
 
-# puts(list.all_e? { |e| e < 5})
-# puts(list.all_e? { |e| e > 5})
-# puts(list.all_e? { |e| e == 2})
-# puts(list.all_e? { |e| e == 5})
-# puts(list.filter_e?(&:even?))
+puts(list.all_e? { |e| e < 5 })
+puts(list.all_e? { |e| e > 5 })
+puts(list.all_e? { |e| e == 2 })
+puts(list.all_e? { |e| e == 5 })
+puts(list.filter_e?(&:even?))
